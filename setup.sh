e8673d78f17b6a440bbdcab14a592649f4d795d0
Akira Tateishi <akira@i-gnote.com>
2021-01-10 11:24:44 +0900

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo yum -y install patch
+
+mkdir ~/.npm-global
+export NPM_CONFIG_PREFIX=~/.npm-global　
+npm install -g aws-cdk
+
+patch ~/.bash_profile bash_profile.patch
+source ~/.bash_profile
